0f18c11ecce64aa73f9b6c4ae799ea82915dcc32
Umut <bozdag321@hotmail.com>
2020-03-19 14:18:55 +0300

    feat: add provider with store

--- src/index.js
+++ src/index.js
@@ -2,5 +2,15 @@ import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
 import "./index.scss";
+import { Provider } from "react-redux";
+import configureStore from "./redux/reducers/configureStore";
+import "react-jinke-music-player/assets/index.css";
 
-ReactDOM.render(<App />, document.getElementById("root"));
+const store = configureStore();
+
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.getElementById("root")
+);
