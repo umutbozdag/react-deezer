6cbdbd6be7e2e071730b94bef369740fac49144d
Umut <bozdag321@hotmail.com>
2020-03-19 14:19:38 +0300

    feat: add routing

--- src/components/Albums/AlbumItem.js
+++ src/components/Albums/AlbumItem.js
@@ -1,26 +1,32 @@
 import React from "react";
 import "./album-item.scss";
 import { LazyLoadImage } from "react-lazy-load-image-component";
+import { Link } from "react-router-dom";
 
 export default function AlbumItem(props) {
   return (
     <div className="album">
       <div className="album__container">
         <div className="album__content">
-          <LazyLoadImage
-            className="album__cover"
-            alt=""
-            src={props.album.cover_medium}
-          />
-          <h4 className="album__title">{props.album.title}</h4>
-          <div className="album__artist">
+          <Link to={`/album/${props.album.id}`}>
             <LazyLoadImage
-              className="album__artist__pic"
+              className="album__cover"
               alt=""
-              src={props.album.artist.picture_small}
+              src={props.album.cover_medium}
             />
-            <p className="album__artist__name">{props.album.artist.name}</p>
-          </div>
+            <h4 className="album__title">{props.album.title}</h4>
+          </Link>
+
+          <Link to={`/artist/${props.album.artist.id}`}>
+            <div className="album__artist">
+              <LazyLoadImage
+                className="album__artist__pic"
+                alt=""
+                src={props.album.artist.picture_small}
+              />
+              <p className="album__artist__name">{props.album.artist.name}</p>
+            </div>
+          </Link>
         </div>
       </div>
     </div>
