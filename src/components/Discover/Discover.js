f304114769a0a6a77b781c10e7d4954e77f05d1e
Umut <bozdag321@hotmail.com>
2020-03-23 20:49:06 +0300

    refactor: remove unnecessary code and add containers

--- src/components/Discover/Discover.js
+++ src/components/Discover/Discover.js
@@ -1,13 +1,37 @@
 import React, { useState, useEffect } from "react";
 import "./discover.scss";
-import Albums from "../Albums";
-import Playlists from "../Playlists";
 import { getUser } from "../../services/deezer";
 import Player from "../Player/Player";
+import PlaylistsContainer from "../../containers/PlaylistsContainer";
+import AlbumsContainer from "../../containers/AlbumsContainer";
+import AppHeader from "../AppHeader/AppHeader";
+import ScrollMenu from "react-horizontal-scrolling-menu";
+import TopTracks from "../TopTracks/TopTracks";
+import TopPlaylists from "../TopPlaylists/TopPlaylists";
+import TopPlaylistsContainer from "../../containers/TopPlaylistsContainer";
+import TopTracksContainer from "../../containers/TopTracksContainer";
 
 export default function Discover(props) {
   const [user, setUser] = useState({});
 
+  const deezerLogin = () => {
+    window.DZ.login(
+      function(response) {
+        console.log(`Auth response: ${JSON.stringify(response.authResponse)}`);
+        if (response.authResponse) {
+          console.log("Welcome!  Fetching your information.... ");
+          window.DZ.api("/user/me", function(response) {
+            localStorage.setItem("user", JSON.stringify(response));
+            // window.location.reload();
+          });
+        } else {
+          console.log("User cancelled login or did not fully authorize.");
+          // localStorage.removeItem("user");
+        }
+      },
+      { perms: "basic_access,email,listening_history" }
+    );
+  };
   useEffect(() => {
     const user = getUser();
     setUser(user);
@@ -15,25 +39,27 @@ export default function Discover(props) {
   }, []);
 
   return (
-    <div className="discover">
-      <div className="discover__container">
-        <div className="discover__content">
-          <h1 className="discover__title">
-            {user ? `Hello ${user.firstname}` : "Discover"}
-          </h1>
-          <p className="discover__desc">
-            Explore music, entertainment and podcast Channels <br /> The music
-            you want. Anywhere, anytime.
-          </p>
-          <button className="discover__button">Explore Deezer Top</button>
+    <div>
+      <AppHeader deezerLogin={deezerLogin} />
+      <div className="discover">
+        <div className="discover__container">
+          <div className="discover__content"></div>
         </div>
-      </div>
-      <div className="discover__trendings">
-        <div className="discover__playlists">
-          <Playlists />
+        <div className="discover__trendings">
+          <div className="discover__playlists">
+            {/* <PlaylistsContainer /> */}
+          </div>
+          <AlbumsContainer />
+          <div className="discover__tops">
+            <TopTracksContainer />
+
+            <TopPlaylistsContainer
+              count={10}
+              shape="rectangle"
+              marginLeft="25%"
+            />
+          </div>
         </div>
-        <Albums />
-        <Player />
       </div>
     </div>
   );
