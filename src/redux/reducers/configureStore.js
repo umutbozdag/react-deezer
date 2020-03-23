e176b88d7e062e97cfeccb6233361fb5c1309860
Umut <bozdag321@hotmail.com>
2020-03-23 20:42:53 +0300

    refactor: refactor for dev tools

--- src/redux/reducers/configureStore.js
+++ src/redux/reducers/configureStore.js
@@ -1,7 +1,13 @@
-import { createStore, applyMiddleware } from "redux";
+import { createStore, applyMiddleware, compose } from "redux";
 import rootReducer from "./index";
 import thunk from "redux-thunk";
 
 export default function configureStore() {
-  return createStore(rootReducer, applyMiddleware(thunk));
+  return createStore(
+    rootReducer,
+    compose(
+      applyMiddleware(thunk),
+      window.devToolsExtension ? window.devToolsExtension() : f => f
+    )
+  );
 }
