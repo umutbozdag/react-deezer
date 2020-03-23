0b323795e4b53531ee82759e887f46cd47dc4f7f
Umut <bozdag321@hotmail.com>
2020-03-23 20:39:23 +0300

    refactor: remove unnecessary methods

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect } from "react";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import Discover from "./components/Discover";
 import Nav from "./components/Nav";
@@ -6,48 +6,31 @@ import "./app.scss";
 import Genres from "./components/Genres";
 import Browse from "./components/Browse/Browse";
 import PlaylistDetail from "./components/PlaylistDetail";
-import { initDeezer, deezerLoginStatus, getUser } from "./services/deezer";
+import { initDeezer } from "./services/deezer";
 import AlbumDetail from "./components/AlbumDetail/AlbumDetail";
 import ArtistDetail from "./components/ArtistDetail/ArtistDetail";
 import Player from "./components/Player/Player";
+import Genre from "./components/Genre/Genre";
 
 function App() {
   useEffect(() => {
     initDeezer();
   }, []);
 
-  const deezerLogin = () => {
-    window.DZ.login(
-      function(response) {
-        if (response.authResponse) {
-          console.log("Welcome!  Fetching your information.... ");
-          window.DZ.api("/user/me", function(response) {
-            localStorage.setItem("user", JSON.stringify(response));
-            window.location.reload();
-          });
-        } else {
-          console.log("User cancelled login or did not fully authorize.");
-        }
-      },
-      { perms: "basic_access,email,listening_history" }
-    );
-  };
-
   return (
     <div className="app">
       <div className="app__container">
         <Router>
-          <Nav deezerLogin={deezerLogin} />
-
+          <Nav />
           <Switch>
-            <Route exact path="/" render={props => <Discover {...props} />} />
+            <Route exact path="/" component={Discover} />
             <Route
               exact
               path="/genres"
               render={props => <Genres {...props} />}
             />
             font-size: 14px;
-            <Route path="/browse" render={props => <Browse {...props} />} />
+            <Route path="/browse" component={Browse} />
             <Route exact path="/playlist/:id" component={PlaylistDetail} />
             <Route
               exact
@@ -55,6 +38,7 @@ function App() {
               render={props => <AlbumDetail {...props} />}
             />
             <Route exact path="/artist/:id" component={ArtistDetail} />
+            <Route exact path="/genre/:id" component={Genre} />
           </Switch>
           <Player />
         </Router>
