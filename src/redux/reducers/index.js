78d479db0bc9b077f82663d165ec1a8814454b66
Umut <bozdag321@hotmail.com>
2020-03-23 20:49:58 +0300

    feat: add new reducers to the store

--- src/redux/reducers/index.js
+++ src/redux/reducers/index.js
@@ -1,10 +1,13 @@
 import { combineReducers } from "redux";
 import { playerReducer } from "./playerReducer";
 import { playlistsReducer } from "./playlistsReducer";
-
+import { albumsReducer } from "./albumsReducer";
+import { topTracksReducer } from "./topTracksReducer";
 const rootReducer = combineReducers({
   playerReducer,
-  playlistsReducer
+  playlistsReducer,
+  albumsReducer,
+  topTracksReducer
 });
 
 export default rootReducer;
