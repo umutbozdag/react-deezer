98eb73ff58cd6d8bea212670b0f358fb5e924265
Umut <bozdag321@hotmail.com>
2020-03-23 20:42:22 +0300

    feat: add arrow components for horizontal menu

---
+++ src/components/Arrow/ArrowLeft.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default function ArrowLeft() {
+  return (
+    <div>
+      <ion-icon name="chevron-back-outline"></ion-icon>
+    </div>
+  );
+}
---
+++ src/components/Arrow/ArrowRight.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default function ArrowRight() {
+  return (
+    <div>
+      <ion-icon name="chevron-forward-outline"></ion-icon>
+    </div>
+  );
+}
