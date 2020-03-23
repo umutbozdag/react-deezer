5b40c2f8142b47c63ec0d5663ec2a7ade2bb5b6a
Umut <bozdag321@hotmail.com>
2020-03-23 20:44:55 +0300

    feat: update albums

--- src/components/Albums/Albums.js
+++ src/components/Albums/Albums.js
@@ -1,30 +1,63 @@
-import React, { useState, useEffect } from "react";
-import axios from "axios";
+import React from "react";
 import AlbumItem from "./AlbumItem";
 import "./albums.scss";
-import { Link } from "react-router-dom";
+import Loading from "../Loading";
+import ScrollMenu from "react-horizontal-scrolling-menu";
 
-export default function Albums() {
-  const [albums, setAlbums] = useState([]);
+export default function Albums(props) {
+  const Arrow = ({ name, className }) => {
+    return (
+      <div className={className}>
+        <ion-icon name={name}></ion-icon>
+      </div>
+    );
+  };
 
-  useEffect(() => {
-    axios
-      .get(
-        "https://cors-anywhere.herokuapp.com/http://api.deezer.com/chart/0/albums&limit=50"
-      )
-      .then(result => {
-        console.log(result);
-        setAlbums(result.data.data);
-      });
-  }, []);
-
+  const ArrowLeft = Arrow({
+    name: "chevron-back-outline",
+    className: "arrow-prev"
+  });
+  const ArrowRight = Arrow({
+    name: "chevron-forward-outline",
+    className: "arrow-next"
+  });
   return (
     <div className="albums">
       <h1 className="albums__title">Trending Albums</h1>
-
       <div className="albums__container">
-        {albums &&
-          albums.map(album => <AlbumItem album={album} key={album.id} />)}
+        <div className="albums__content">
+          {props.isPending ? (
+            <div className="albums__loading">LOADING</div>
+          ) : (
+            <ScrollMenu
+              arrowClass="arrow"
+              itemClass="CardOffset"
+              alignCenter={false}
+              data={
+                props.albums &&
+                props.albums.map(album => (
+                  <AlbumItem album={album} key={album.id} />
+                ))
+              }
+              arrowRight={ArrowRight}
+              arrowLeft={ArrowLeft}
+              dragging={true}
+              wheel={true}
+              alignOnResize={true}
+            />
+          )}
+
+          {/* {props.isPending ? (
+            <div>
+              <Loading />
+            </div>
+          ) : (
+            props.albums &&
+            props.albums.map(album => (
+              <AlbumItem album={album} key={album.id} />
+            ))
+          )} */}
+        </div>
       </div>
     </div>
   );
