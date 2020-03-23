a0c0ec4c83f469da433698a4a4b102cd3c180759
Umut <bozdag321@hotmail.com>
2020-03-23 20:48:44 +0300

    feat: add Links

--- src/components/Browse/Browse.js
+++ src/components/Browse/Browse.js
@@ -1,9 +1,28 @@
 import React from "react";
 import "./browse.scss";
+import { Link } from "react-router-dom";
 
 export default function Browse() {
   return (
     <div className="browse">
+      <div className="browse__container">
+        <div className="browse__nav">
+          <div className="browse__nav__items">
+            <Link className="browse__nav__item" to="/albums">
+              Albums
+            </Link>
+            <Link className="browse__nav__item" to="/artists">
+              Artists
+            </Link>
+            <Link className="browse__nav__item" to="/genres">
+              Genres
+            </Link>
+            <Link className="browse__nav__item" to="/genres">
+              New Releases
+            </Link>
+          </div>
+        </div>
+      </div>
       <h2>Browse</h2>
     </div>
   );
