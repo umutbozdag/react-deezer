871b1cde4994acd9b46d206c107bbb420d77ad65
Umut <bozdag321@hotmail.com>
2020-03-23 20:46:35 +0300

    refactor: refactor code

--- src/components/Albums/AlbumItem.js
+++ src/components/Albums/AlbumItem.js
@@ -4,26 +4,28 @@ import { LazyLoadImage } from "react-lazy-load-image-component";
 import { Link } from "react-router-dom";
 
 export default function AlbumItem(props) {
+  const getWords = (str, count) => {
+    return str
+      .split(" ")
+      .slice(0, count)
+      .join(" ");
+  };
+
   return (
     <div className="album">
       <div className="album__container">
         <div className="album__content">
-          <Link to={`/album/${props.album.id}`}>
+          <Link className="album__link" to={`/album/${props.album.id}`}>
             <LazyLoadImage
               className="album__cover"
               alt=""
               src={props.album.cover_medium}
             />
-            <h4 className="album__title">{props.album.title}</h4>
+            <h4 className="album__title">{getWords(props.album.title, 3)}</h4>
           </Link>
 
-          <Link to={`/artist/${props.album.artist.id}`}>
+          <Link className="album__link" to={`/artist/${props.album.artist.id}`}>
             <div className="album__artist">
-              <LazyLoadImage
-                className="album__artist__pic"
-                alt=""
-                src={props.album.artist.picture_small}
-              />
               <p className="album__artist__name">{props.album.artist.name}</p>
             </div>
           </Link>
