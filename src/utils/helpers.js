2775b05780b1123be9ceaf1fc096e2f35654596f
Umut <bozdag321@hotmail.com>
2020-03-23 20:39:49 +0300

    feat: add new helper

--- src/utils/helpers.js
+++ src/utils/helpers.js
@@ -3,3 +3,14 @@ export const getFirstWord = str => {
   return spaceIndex === -1 ? str : str.substr(0, spaceIndex);
 };
 export const shuffle = () => {};
+
+export const changeTheme = () => {
+  const html = document.querySelector("html");
+  if (html.classList.contains("light")) {
+    html.classList.add("dark");
+    html.classList.remove("light");
+  } else if (html.classList.contains("dark")) {
+    html.classList.add("light");
+    html.classList.remove("dark");
+  }
+};
