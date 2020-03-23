697f878d1ff9866e7055f63e3392e70ed4213fe4
Umut <bozdag321@hotmail.com>
2020-03-23 20:44:14 +0300

    refactor: refactor code

--- src/components/AlbumDetail/AlbumDetail.js
+++ src/components/AlbumDetail/AlbumDetail.js
@@ -1,22 +1,18 @@
 import React, { useEffect, useState } from "react";
 import axios from "axios";
 import { API_URL } from "../../utils/url";
-import { useDispatch, useSelector } from "react-redux";
-import * as actionTypes from "../../redux/actions/actionTypes";
+import TrackItem from "../TrackItem/TrackItem";
+import { Link } from "react-router-dom";
 
 export default function AlbumDetail(props) {
   const [albumDetail, setAlbumDetail] = useState([]);
 
-  const dispatch = useDispatch();
-
   useEffect(() => {
-    axios
-      .get(`${API_URL}/album/${props.match.params.id}/tracks`)
-      .then(result => {
-        setAlbumDetail(result.data.data);
-        console.log(albumDetail);
-        console.log(result);
-      });
+    axios.get(`${API_URL}/album/${props.match.params.id}`).then(result => {
+      setAlbumDetail(result.data);
+      console.log(result);
+      console.log(albumDetail);
+    });
   }, []);
 
   return (
@@ -24,24 +20,18 @@ export default function AlbumDetail(props) {
       <h1>Album Detail of {props.match.params.id}</h1>
 
       <h2>Album tracks</h2>
+      <h3>{albumDetail.title}</h3>
+      <img src={albumDetail.cover_big} alt="" />
 
-      {albumDetail.map(track => (
-        <div>
-          <div key={track.id}>
-            <h1>{track.title}</h1>
-            <button
-              onClick={() =>
-                dispatch({
-                  type: actionTypes.SET_ACTIVE_SOUND,
-                  payload: track
-                })
-              }
-            >
-              Set music
-            </button>
-          </div>
-        </div>
-      ))}
+      <h4>Genres</h4>
+      {albumDetail.genres &&
+        albumDetail.genres.data.map(genre => (
+          <Link to={`/genre/${genre.id}`}>{genre.name}</Link>
+        ))}
+      {albumDetail.tracks &&
+        albumDetail.tracks.data.map(track => (
+          <TrackItem track={track} key={track.id} />
+        ))}
     </div>
   );
 }
