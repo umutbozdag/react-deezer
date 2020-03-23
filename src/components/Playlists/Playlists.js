b9b716ffdff1e759de0ccfe987f854ce62b03450
Umut <bozdag321@hotmail.com>
2020-03-23 20:49:20 +0300

    refactor: refactor

--- src/components/Playlists/Playlists.js
+++ src/components/Playlists/Playlists.js
@@ -1,34 +1,25 @@
-import React, { useEffect, useState } from "react";
-import axios from "axios";
+import React from "react";
 import "./playlists.scss";
-import { API_URL } from "../../utils/url";
 import PlaylistItem from "./PlaylistItem";
 import Loading from "../Loading";
-import { useDispatch, useSelector } from "react-redux";
-import { getTopPlaylists } from "../../redux/actions/apiActions";
 
 export default function Playlists(props) {
-  // const [playlists, setPlaylists] = useState([]);
-
-  const dispatch = useDispatch();
-  const playlists = useSelector(
-    state =>
-      state.playlistsReducer.playlists &&
-      state.playlistsReducer.playlists.data.data
-  );
-  console.log(playlists);
-
-  useEffect(() => {
-    dispatch(getTopPlaylists());
-  }, []);
-
+  console.log(props);
   return (
     <div className="playlists">
       <div className="playlists__container">
-        {playlists &&
-          playlists.map(playlist => (
-            <PlaylistItem playlist={playlist} key={playlist.id} />
-          ))}
+        {props.isPending ? (
+          <Loading />
+        ) : (
+          props.playlists &&
+          props.playlists.map(playlist => (
+            <PlaylistItem
+              shape={props.shape}
+              playlist={playlist}
+              key={playlist.id}
+            />
+          ))
+        )}
       </div>
     </div>
   );
